f1476e4e2c6e2e55a3992c708de465bd0268fc30
P.S.V.R <pmq2001@gmail.com>
2012-08-30 21:08:02 +0800

    艰难的css修复

--- sub/app/helpers/application_helper.rb
+++ sub/app/helpers/application_helper.rb
@@ -1,5 +1,17 @@
 # coding: utf-8
 module ApplicationHelper
+  def kaixue_msg
+    day=(Time.now-$cnu_new)/86400
+    day=day.to_i-1
+    if day < 0
+      return "今天离开学还有#{-day}天"
+    else
+      return "今天是开学第#{day}天"
+    end
+  end
+  def cpath(c)
+    "/simple/forum.php?mod=forumdisplay&fid=#{c.fid}"
+  end
   def timeago(time, options = {})
     options[:class]
     options[:class] = options[:class].blank? ? "timeago" : [options[:class],"timeago"].join(" ")
