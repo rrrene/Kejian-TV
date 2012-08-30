0933d42757ef6d35548d683c5fb38608408f05af
P.S.V.R <pmq2001@gmail.com>
2012-08-30 18:28:25 +0800

    准备混合求助asset

--- sub/app/controllers/home_controller.rb
+++ sub/app/controllers/home_controller.rb
@@ -2,10 +2,8 @@
 class HomeController < ApplicationController
   before_filter :require_user_text, :only => [:update_in_place,:mute_suggest_item]
   before_filter :authenticate_user!, :except => [:newbie,:about,:index,:general_show,:agreement,:mobile]
-  before_filter :we_are_inside_qa
-  def we_are_inside_qa
-    @we_are_inside_qa = true
-  end
+  layout :for_help
+  #before_filter :we_are_inside_qa
 
   def mobile
     render :layout=>false
