5780cfddd50fcb8e65ebfe12f18577fbde3fabf1
P.S.V.R <pmq2001@gmail.com>
2012-09-04 00:07:06 +0800

    加入不显示dz已删除内容的scope，合并

--- sub/app/controllers/welcome_controller.rb
+++ sub/app/controllers/welcome_controller.rb
@@ -3,23 +3,23 @@ class WelcomeController < ApplicationController
   def index
     @seo[:title] = '首页'
     @stat = PreCommonStat.order
-    @courses = PreForumForum.order('threads desc').limit(10)
-    @coursewares = PreForumThread.order('views desc').limit(10)
-    @coursewares1 = PreForumThread.where('dateline>=?',Date.today.at_beginning_of_day.to_i).order('views desc')
+    @courses = PreForumForum.nondeleted.order('threads desc').limit(10)
+    @coursewares = PreForumThread.nondeleted.order('views desc').limit(10)
+    @coursewares1 = PreForumThread.nondeleted.where('dateline>=?',Date.today.at_beginning_of_day.to_i).order('views desc')
     @cwyesterday = PreForumThread.where('dateline>=? and dateline <?',Date.yesterday.at_beginning_of_day.to_i,Date.today.at_beginning_of_day.to_i).count
     @cw = PreForumThread.count
     @users = PreCommonMember.count
     @newuser =  PreCommonMember.order('regdate').last
     if @coursewares1.count > 0
-      @coursewares2 = PreForumThread.where('dateline<?',@coursewares1[-1].dateline).order('dateline desc').first
+      @coursewares2 = PreForumThread.nondeleted.where('dateline<?',@coursewares1[-1].dateline).order('dateline desc').first
     else
-      @coursewares2 = PreForumThread.order('dateline desc').first
+      @coursewares2 = PreForumThread.nondeleted.order('dateline desc').first
     end
     if @coursewares2
-      @coursewares2 = PreForumThread.where('dateline<=? and dateline>=?',Time.at(@coursewares2.dateline).to_i,Time.at(@coursewares2.dateline).at_beginning_of_day.to_i).order('dateline desc')
-      @coursewares3 = PreForumThread.where('dateline<?',@coursewares2[-1].dateline).order('dateline desc').first
+      @coursewares2 = PreForumThread.nondeleted.where('dateline<=? and dateline>=?',Time.at(@coursewares2.dateline).to_i,Time.at(@coursewares2.dateline).at_beginning_of_day.to_i).order('dateline desc')
+      @coursewares3 = PreForumThread.nondeleted.where('dateline<?',@coursewares2[-1].dateline).order('dateline desc').first
       if @coursewares3
-        @coursewares3 = PreForumThread.where('dateline<=? and dateline>=?',Time.at(@coursewares3.dateline).to_i,Time.at(@coursewares3.dateline).at_beginning_of_day.to_i).order('dateline desc')
+        @coursewares3 = PreForumThread.nondeleted.where('dateline<=? and dateline>=?',Time.at(@coursewares3.dateline).to_i,Time.at(@coursewares3.dateline).at_beginning_of_day.to_i).order('dateline desc')
       end
     end
   end
