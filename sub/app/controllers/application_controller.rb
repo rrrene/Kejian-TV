17b8d13011904fd82affe3d3af5cea907fade7bf
P.S.V.R <pmq2001@gmail.com>
2012-09-04 16:28:35 +0800

    小错误

--- sub/app/controllers/application_controller.rb
+++ sub/app/controllers/application_controller.rb
@@ -71,7 +71,7 @@ class ApplicationController < ActionController::Base
   end
 
   def dz_security
-    @authkey = UCenter::Php.md5(Setting.dz_authkey+cookies[Discuz.cookiepre_real+'saltkey'])
+    @authkey = UCenter::Php.md5("#{Setting.dz_authkey}#{cookies[Discuz.cookiepre_real+'saltkey']}")
     if user_signed_in?
       @formhash = Discuz::Utils.formhash({'username'=>current_user.slug,'uid'=>current_user.uid,'authkey'=>@authkey})
     else
