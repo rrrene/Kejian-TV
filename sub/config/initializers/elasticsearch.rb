7ec74500713fddb45765be47441d697d51e5657e
P.S.V.R <pmq2001@gmail.com>
2012-10-09 16:24:11 +0800

    正在搞搜索

---
+++ sub/config/initializers/elasticsearch.rb
@@ -0,0 +1,14 @@
+# -*- encoding : utf-8 -*-
+unless $psvr_really_development
+
+( puts <<-INSTALL
+ [ERROR] You don’t appear to have ElasticSearch installed. Please install and launch it with the following commands:
+
+  curl -k -L -o elasticsearch-0.17.6.tar.gz http://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.17.6.tar.gz
+   tar -zxvf elasticsearch-0.17.6.tar.gz
+    ./elasticsearch-0.17.6/bin/elasticsearch -f
+INSTALL
+exit(1) 
+) unless (RestClient.get('http://0.0.0.0:9200') rescue false)
+
+end
--- sub/config/initializers/z_ktv.rb
+++ sub/config/initializers/z_ktv.rb
@@ -83,10 +83,10 @@ end
 UCenter.define('UC_CLIENT_RELEASE', '20110501')
 UCenter.define('UC_APPID', Setting.uc_appid);
 UCenter.define('UC_KEY', Setting.uc_key);
-unless Rails.env.sub_cnu_dev? or Rails.env.sub_ibeike_dev?
-  UCenter.define('UC_API', 'http://uc.kejian.tv');
-else
+if $psvr_really_development
   UCenter.define('UC_API', 'http://uc.kejian.lvh.me');
+else
+  UCenter.define('UC_API', 'http://uc.kejian.tv');
 end
 
 Ktv.configure do |config|
