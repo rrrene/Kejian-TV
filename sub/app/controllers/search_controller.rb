29d944ec851092af7ab251e971d13917579c6e1e
P.S.V.R <pmq2001@gmail.com>
2012-10-11 18:12:28 +0800

    下午优化的查询字符串

--- sub/app/controllers/search_controller.rb
+++ sub/app/controllers/search_controller.rb
@@ -9,16 +9,19 @@ class SearchController < ApplicationController
   end
   def show
     @seo[:title] = params[:q]
-    params[:page]||='1'
-    params[:per_page]||='15'
-    @bm = Benchmark.measure {
-      params_q = params[:q].xi
-      @pages = Page.search(:page=> params[:page], :per_page=> params[:per_page]) do
-        query do
-          string params_q, default_operator: "AND"
-        end
-      end
-    }.total
+    @page = params[:page].to_i
+    unless @page > 0
+      @page = 1
+      params[:page] = @page.to_s
+    end
+    params[:per_page] ||= cookies[:welcome_per_page]
+    @per_page = params[:per_page].to_i
+    unless @per_page > 0
+      @per_page = 15 
+      params[:per_page] = @per_page.to_s
+    end
+    cookies[:welcome_per_page] = @per_page.to_s
+    @pages = Page.psvr_search(@page,@per_page,params)
     redirect_to "/coursewares/#{@pages.first.courseware_id}" if request.path.split('/')[0].ends_with?('lucky')
   end
 end
--- sub/app/helpers/coursewares_helper.rb
+++ sub/app/helpers/coursewares_helper.rb
@@ -6,4 +6,7 @@ module CoursewaresHelper
     return current_user.thanked_courseware_ids.count(courseware.id) > 0
     return
   end
+  def page_highlight(html)
+    return html_escape(html).gsub('__PSVR_begin_RVSP__','<span class="search-highlight">').gsub('__PSVR_end_RVSP__','</span>').html_safe
+  end
 end
