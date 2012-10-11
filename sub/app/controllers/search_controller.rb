1045b68eeeef46093775fd7e2df06e7f0ec16dbe
P.S.V.R <pmq2001@gmail.com>
2012-10-11 11:47:28 +0800

    课件也要单独索引

--- sub/app/controllers/search_controller.rb
+++ sub/app/controllers/search_controller.rb
@@ -2,9 +2,23 @@
 class SearchController < ApplicationController
   def index
     @seo[:title] = '课件搜索'
+    if params[:q].present?
+      redirect_to "/search/#{params[:q]}"
+      return false
+    end
   end
   def show
     @seo[:title] = params[:q]
-    redirect_to '/' if request.path.split('/')[0].ends_with?('lucky')
+    params[:page]||='1'
+    params[:per_page]||='15'
+    @bm = Benchmark.measure {
+      params_q = params[:q].xi
+      @pages = Page.search(:page=> params[:page], :per_page=> params[:per_page]) do
+        query do
+          string params_q, default_operator: "AND"
+        end
+      end
+    }.total
+    redirect_to "/coursewares/#{@pages.first.courseware_id}" if request.path.split('/')[0].ends_with?('lucky')
   end
 end
