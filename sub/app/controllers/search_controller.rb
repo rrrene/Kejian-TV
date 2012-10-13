587aa441dafd0603c7f748a0df502348cc74e22f
P.S.V.R <pmq2001@gmail.com>
2012-10-13 17:53:04 +0800

    搜索基本上穿起来了

--- sub/app/controllers/search_controller.rb
+++ sub/app/controllers/search_controller.rb
@@ -8,6 +8,7 @@ class SearchController < ApplicationController
     end
   end
   def lucky
+    search_common_op
     @pages = Page.psvr_search(@page,@per_page,params)
     redirect_to "/coursewares/#{@pages.first.courseware_id}"
     return false
@@ -15,12 +16,17 @@ class SearchController < ApplicationController
   def show
     search_common_op
     @coursewares = Courseware.psvr_search(@page,@per_page,params)
+    @quans=@coursewares
+    @quan='个'
+    @thing='课件'
+    @mode=:kejian
     respond_to do |format|
       format.html
       format.json{
         render json:{
+          term:@user_provided_term,
           content:render_to_string(:file=>'search/_show.html.erb',:layout=>nil, :formats=>[:html]),
-          main:render_to_string(:file=>'search/_search_show_common_main.html.erb',:layout=>nil,:locals=>{:quans=>@coursewares,:quan=>'个',:thing=>'课件',:mode=>:kejian}, :formats=>[:html])
+          main:render_to_string(:file=>'search/_search_show_common_main.html.erb',:layout=>nil, :formats=>[:html])
         }
       }
     end
@@ -28,6 +34,20 @@ class SearchController < ApplicationController
   def show_contents
     search_common_op
     @pages = Page.psvr_search(@page,@per_page,params)
+    @quans=@pages
+    @quan='页'
+    @thing='课件内容'
+    @mode=:kejianneirong
+    respond_to do |format|
+      format.html
+      format.json{
+        render json:{
+          term:@user_provided_term,
+          content:render_to_string(:file=>'search/_show_contents.html.erb',:layout=>nil, :formats=>[:html]),
+          main:render_to_string(:file=>'search/_search_show_common_main.html.erb',:layout=>nil, :formats=>[:html])
+        }
+      }
+    end
   end
   def show_playlists
     search_common_op
@@ -43,6 +63,7 @@ class SearchController < ApplicationController
   end
 private
   def search_common_op
+    @user_provided_term=params[:q]
     @using_ajax = request.path.split('/')[1]=='ajax'
     @seo[:title] = params[:q]
     @page = params[:page].to_i
@@ -50,13 +71,13 @@ private
       @page = 1
       params[:page] = @page.to_s
     end
-    params[:per_page] ||= cookies[:welcome_per_page]
+    params[:per_page] ||= cookies[:search_per_page]
     @per_page = params[:per_page].to_i
     unless @per_page > 0
-      @per_page = 15 
+      @per_page = 10 
       params[:per_page] = @per_page.to_s
     end
-    cookies[:welcome_per_page] = @per_page.to_s
+    cookies[:search_per_page] = @per_page.to_s
     params[:q]=params[:q].xi
   end
 end
