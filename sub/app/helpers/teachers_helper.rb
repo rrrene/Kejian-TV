2ce2591eb8e7c290b98d50d12242c179f64297b8
P.S.V.R <pmq2001@gmail.com>
2012-10-15 10:12:07 +0800

    宽窄转换修复

--- sub/app/helpers/teachers_helper.rb
+++ sub/app/helpers/teachers_helper.rb
@@ -12,4 +12,16 @@ module TeachersHelper
       url = "#{Setting.upload_url}/teacher/avatar/#{teacher}/#{size}_#{url}"
     end
   end
+  def teacher_avatar_url(teacher,size=:normal)
+    s=AvatarUploader::SIZES[size]
+    url = teacher.avatar_filename
+    if url.blank?
+      url = "/defaults/avatar/#{size}.png"
+    elsif url.starts_with?('http://')
+      d = CGI::escape("/defaults/avatar/#{size}.png")
+      url = "#{url}?r=PG&s=#{s}&d=#{d}"
+    else
+      url = "#{Setting.upload_url}/teacher/avatar/#{teacher.name}/#{size}_#{url}"
+    end
+  end
 end
