b438c8f2bc3b362e91e8922ee277438991569100
Liber <llb0536@gmail.com>
2012-10-15 22:21:26 +0800

    view_all_playlists  Firefox那个兼容性bug搞定了

--- sub/app/controllers/play_lists_controller.rb
+++ sub/app/controllers/play_lists_controller.rb
@@ -6,4 +6,25 @@ class PlayListsController < ApplicationController
     @play_lists = PlayList.no_privacy.normal
     @play_lists = @play_lists.paginate(:page => params[:page], :per_page => @per_page)
   end
+  
+  def new
+    @play_list = PlayList.new
+  end
+  
+  def edit
+    @play_list = PlayList.find(params[:id])
+  end
+  
+  def show
+    @play_list = PlayList.find(params[:id])
+    @seo[:title] = "课件锦囊"    
+  end
+  
+  def create
+    
+  end
+  
+  def update
+    
+  end
 end
