5ba0e722ffc3f2fffa399ccbb57f81774726a859
PSVR <pmq2001@gmail.com>
2012-10-18 13:59:32 +0800

    本地的修改

--- sub/app/controllers/application_controller.rb
+++ sub/app/controllers/application_controller.rb
@@ -2,9 +2,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter proc{
-    puts request.env['HTTP_USER_AGENT']+request.ip
-    puts ' '
-    puts request.path
+    # puts request.env['HTTP_USER_AGENT']+request.ip
+    # puts ' '
+    # puts request.path
     # text = request.user_agent    
     # render text:text and return
   }
