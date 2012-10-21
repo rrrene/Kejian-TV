912aeb7d28e40417940a18bb5a42dd2fe2b2be5e
P.S.V.R <pmq2001@gmail.com>
2012-10-21 15:14:48 +0800

    为了修正线上bug，comments的render有问题

--- sub/app/controllers/comments_controller.rb
+++ sub/app/controllers/comments_controller.rb
@@ -10,9 +10,6 @@ class CommentsController < ApplicationController
         @comments = Comment.where(:commentable_type => @type.titleize, :commentable_id => BSON::ObjectId(@id)).nondeleted.desc("created_at").to_a
         @comments = @comments.paginate(:page => params[:page], :per_page => @per_page)
         @comment = Comment.new(:commentable_type => @type.titleize, :commentable_id => @id)
-        respond_to do |format|
-          format.any{render file:'comments/index.js.erb'}
-        end
     else
         redirect_to '/'
     end
