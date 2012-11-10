a6b6fe55f9da6b6b364dc509757fee2506019914
Liber <llb0536@gmail.com>
2012-11-10 18:20:27 +0800

    jiathis 分享课件锦囊

--- sub/app/controllers/ajax_controller.rb
+++ sub/app/controllers/ajax_controller.rb
@@ -1087,7 +1087,7 @@ HEREDOC
   end
   def get_playlist_share
       url = "http://#{Setting.ktv_sub.nil? ? 'www' : Setting.ktv_sub}.kejian#{$psvr_really_development ? '.lvh.me' : '.tv'}/play_lists/#{params[:playlist_id]}"
-      render file:'play_lists/_playlist_share',locals:{url:url},layout:false
+      render file:'play_lists/_playlist_share',locals:{url:url,title:params[:title]},layout:false
   end
   def like_playlist
     return false if current_user.nil?
