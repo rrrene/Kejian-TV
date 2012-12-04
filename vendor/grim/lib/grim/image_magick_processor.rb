1e86c243474998d20b0ade49dc3ace4b6e6cc930
llb0536 <llb0536@gmail.com>
2012-12-04 11:57:41 +0800

    grim 还是自己写的靠谱

---
+++ vendor/grim/lib/grim/image_magick_processor.rb
@@ -0,0 +1,51 @@
+module Grim
+  class ImageMagickProcessor
+
+    # ghostscript prints out a warning, this regex matches it
+    WarningRegex = /\*\*\*\*.*\n/
+
+    def initialize(options={})
+      @imagemagick_path = options[:imagemagick_path] || 'convert'
+      @ghostscript_path = options[:ghostscript_path]
+      @original_path        = ENV['PATH']
+    end
+
+    def count(path)
+      command = ["-dNODISPLAY", "-q",
+        "-sFile=#{Shellwords.shellescape(path)}",
+        File.expand_path('../../../lib/pdf_info.ps', __FILE__)]
+      @ghostscript_path ? command.unshift(@ghostscript_path) : command.unshift('gs')
+      result = `#{command.join(' ')}`
+      result.gsub(WarningRegex, '').to_i
+    end
+
+    def save(pdf, index, path, options, processor_options)
+      width   = options.fetch(:width,   Grim::WIDTH)
+      density = options.fetch(:density, Grim::DENSITY)
+      quality = options.fetch(:quality, Grim::QUALITY)
+      resize  = options.fetch(:resize,  Grim::RESIZE)
+      command = [@imagemagick_path, "-flatten", "-antialias", "-render",
+        "-quality", quality.to_s,
+        "-interlace", "none", "-density", density.to_s,
+        "#{Shellwords.shellescape(pdf.path)}[#{index}]", path]
+
+      if resize
+        command.insert(2, "-resize")
+        command.insert(3, width.to_s)
+      end
+
+      if processor_options.any?
+        processor_options.each_pair do |key, value|
+          command.insert(10, "-#{key.to_s}")
+          command.insert(11, value.is_a?(String) ? "#{value}" : value )
+        end
+      end
+
+      command.unshift("PATH=#{File.dirname(@ghostscript_path)}:#{ENV['PATH']}") if @ghostscript_path
+
+      result = `#{command.join(' ')}`
+
+      $? == 0 || raise(UnprocessablePage, result)
+    end
+  end
+end
