40e9132dd12299191aff5d2352c8298f99569ad2
llb0536 <llb0536@gmail.com>
2012-12-11 13:48:19 +0800

    是资源不是课件，记得将来写dz的防普通用户越界测试

--- sub/test/integration/dz_integration_test.rb
+++ sub/test/integration/dz_integration_test.rb
@@ -2,8 +2,8 @@
 require "test_helper"
 
 # To be handled correctly this spec must end with "Acceptance Test"
-describe "先发制人人系列功能 Acceptance Test" do
-  it "must be a real test" do
+describe "DZ系列功能 Acceptance Test" do
+  it "必须不能让普通用户看到dz的内容" do
     flunk "Need real tests"
   end
 end
