5ae134db5ab2a3cd3b0a42c80b817185ff2a980b
P.S.V.R <pmq2001@gmail.com>
2014-03-24 01:00:00 +0800

    Rails new with 4.0.4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KejianTv::Application.config.secret_key_base = '0c609e9df74bd1b940c92bf27303309fc9e8b4ebdfa077f82f3034a174fbc9e348064302c64b4482a0eafdd51b90251a032e8d9f75de3ec3cdc9a52511404427'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KejianTv::Application.config.session_store :cookie_store, key: '_Kejian-TV_session'
