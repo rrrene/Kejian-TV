e52377ae85894d16f982452bab3237d5127f78d0
P.S.V.R <pmq2001@gmail.com>
2014-04-08 01:00:00 +0800

    Fix asset file reference

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module KejianTv
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :'zh-CN'
+    
+    config.assets.precompile += %w( wl/home.css wl/weblab/browserRedirect.js wl/homeLoader.js wl/home.js )
   end
 end
 
