668a51e288035a2a4c11adcb5bf4fad92941d334
P.S.V.R <pmq2001@gmail.com>
2014-04-08 01:00:00 +0800

    Public inn

---
+++ public/DOM-shim/src/all/interfaces/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+	CustomEvent: require("./CustomEvent"),
+	Element: require("./Element"),
+	HTMLElement: require("./HTMLElement"),
+	HTMLLabelElement: require("./HTMLLabelElement"),
+	Event: require("./Event"),
+	EventTarget: require("./EventTarget"),
+	Node: require("./Node")
+};
